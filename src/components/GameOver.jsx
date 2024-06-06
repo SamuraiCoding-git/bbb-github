16f9dbaaec6d2d5755100ad55c2f79d36b756821
Tim Timoshkin <timoshkin.timofey@gmail.com>
2024-06-06 22:18:42 +0300

    birb
    bruh

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 import DurovBackground from "../assets/page.svg";
 import ExitButtonImage from "../assets/img/Durov/exit.png";
+import Dead from "../assets/img/Dead/dead.svg";
+import Restart from "../assets/img/Dead/restart.svg";
+import Scull from "../assets/img/Dead/scull.svg";
 
-const GameOver = () => {
+const GameOver = ({ onRestart }) => {
     const navigate = useNavigate();
+    const gameOverRef = useRef(null);
 
     const goToMain = () => {
         navigate('/');
     };
 
+    useEffect(() => {
+        const gameOverElement = gameOverRef.current;
+        if (gameOverElement) {
+            gameOverElement.style.transform = 'translateY(-100%)';
+            gameOverElement.style.transition = 'transform 0.5s ease-in-out';
+            requestAnimationFrame(() => {
+                gameOverElement.style.transform = 'translateY(0)';
+            });
+        }
+    }, []);
+
     return (
         <div
-            className="relative max-w-[510px] h-screen w-screen mx-auto bg-no-repeat bg-center block bottom-[50px]"
+            ref={gameOverRef}
+            className="absolute max-w-[510px] h-screen w-screen mx-auto bg-no-repeat bg-center block bottom-[50px] z-40"
             style={{
                 backgroundImage: `url(${DurovBackground})`,
-                backgroundSize: '404px 404px', // Set specific dimensions
+                backgroundSize: '404px 404px',
+                transform: 'translateY(-100%)',
             }}
         >
+            <div className="flex h-[100dvh] justify-center items-center flex-col space-y-4">
+                <img className="absolute w-64 h-64 z-50" src={Scull} alt="Scull" />
+                <img className="absolute w-72 z-50" style={{backgroundSize: '150px'}} src={Dead} alt="Dead" />
+                <img
+                    className="w-72 z-50 cursor-pointer absolute top-1/2 -translate-y-[50%]"
+                    src={Restart}
+                    alt="Restart"
+                    onClick={onRestart}
+                />
+            </div>
             <img
                 src={ExitButtonImage}
                 alt="Exit"
--- src/pages/Invite.jsx
+++ src/pages/Invite.jsx
@@ -4,6 +4,21 @@ import InviteFriendsButton from "../assets/img/Invite/invite_friends_button.svg"
 import InviteTermsWindow from "../assets/img/Invite/window_terms_for_refferals.svg"
 
 const Invite = () => {
+
+    const openExternalLink = (link) => {
+        if (window.Telegram && window.Telegram.WebApp) {
+            window.Telegram.WebApp.openLink(link);
+        }
+    };
+
+    const getUserId = () => {
+        if (window.Telegram && window.Telegram.WebApp) {
+            const tg = window.Telegram.WebApp;
+            return tg.initDataUnsafe?.user?.id;
+        }
+        return null;
+    };
+
     return (
         <div className="relative flex flex-col items-center justify-start h-screen w-screen max-w-[574px]">
             <header className="flex justify-end w-full p-4">
@@ -12,18 +27,29 @@ const Invite = () => {
                 </div>
             </header>
             <div className="relative flex flex-col items-center justify-start flex-1 mt-16">
-                <img src={InviteWindow} alt="Invite window illustration" className="w-[392px] h-[349px]" />
-                <div className="absolute w-[248px] h-[54px]" style={{ top: '45%' }}>
-                    <img
-                        src={InviteFriendsButton}
-                        alt="Invite Friends"
-                        className="w-full h-full"
-                    />
-                    <div className="absolute inset-0 flex items-center justify-center text-black font-bold" style={{ fontSize: '24px' }}>
-                        Invite
-                    </div>
+                <div>
+                    <img src={InviteWindow} alt="Invite window illustration" className="w-[392px] h-[349px]" />
                 </div>
-                <img src={InviteTermsWindow} alt="" className="absolute w-[265px] h-[108px] top-[50px]"></img>
+                    <div className="absolute top-[40px] text-3xl">My referrals</div>
+                    <div className="absolute w-[265px] h-[108px] top-[100px]">
+                            <div className="z-20 text-xl absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-nowrap">
+                                <b>Bro = 150 $BALLS <br />
+                                Bro’s buddy = 50 $BALLS
+                                </b>
+                            </div>
+                            <img src={InviteTermsWindow} alt="" className="absolute w-[265px] h-[108px] top-0 z-10" />
+                    </div>
+                    
+                    <button onClick={openExternalLink(`https://t.me/share/url?text=&url=https://t.me/big_balls_birds_bot?start=${getUserId()}`)} className="absolute w-[248px] h-[54px] top-[250px]">
+                            <img
+                                src={InviteFriendsButton}
+                                alt="Invite Friends"
+                                className="w-full h-full"
+                            />
+                        <div className="absolute inset-0 flex items-center justify-center text-black font-bold" style={{ fontSize: '24px' }}>
+                            Invite
+                        </div>
+                    </button>
             </div>
         </div>
     );
