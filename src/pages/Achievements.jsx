1441286db53450c4a68d5d807bf3730bc86a9b08
K.Darlin <konstantdarl@gmail.com>
2024-06-08 22:30:10 +0300

    Remade Achivement layout

--- src/pages/Achievements.jsx
+++ src/pages/Achievements.jsx
@@ -1,10 +1,10 @@
-import AchievementsImage from "../assets/achievement-window.svg";
-import ConcealedAchievementsImage from "../assets/achievement-with-picture-space.svg";
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import BackPaperImage from "../assets/back-paper-L.svg";
 import { HeaderContext } from "../components/Header";
 
 const Achievements = () => {
     const { setIsShowCloseBtn } = useContext(HeaderContext)
+    const [achivments, setAchivments] = useState([1,2,3,4,5,6])
 
     useEffect(() => {
         setIsShowCloseBtn(true)
@@ -13,24 +13,29 @@ const Achievements = () => {
     }, [])
 
     return (
-        <div className="max-w-[720px] mt-[15px] ml-[195px]">
-            <div className="achievements-header">
-                <span>
+        <div className="relative h-screen w-screen">
+            <div className="absolute left-1/2 transform -translate-x-1/2 mt-36">
+                <img src={BackPaperImage} className="absolute w-[310px] left-1/2 transform -translate-x-1/2"/>
+                <span className="relative text-3xl top-10 left-1/2 transform -translate-x-1/2">
                     ???
                 </span>
-            </div>
-            <div className="achievements-container">
-                <div className="achievements-window">
-                    <img src={AchievementsImage} alt=""/>
-                </div>
-                <div className="achievements-list">
-                    <img src={ConcealedAchievementsImage} className="concealed-image" alt=""></img>
-                    <img src={ConcealedAchievementsImage} className="concealed-image" alt=""></img>
-                    <img src={ConcealedAchievementsImage} className="concealed-image" alt=""></img>
-                    <img src={ConcealedAchievementsImage} className="concealed-image" alt=""></img>
-                    <img src={ConcealedAchievementsImage} className="concealed-image" alt=""></img>
-                    <img src={ConcealedAchievementsImage} className="concealed-image" alt=""></img>
-                </div>
+                <ul className="relative mt-12 w-[310px] text-2xl">
+                    {achivments.map((member, index) => (
+                        <li className="relative h-fit ">
+                            <div className="relative flex flex-row justify-between items-center py-2 px-6">
+                                <div className="w-12 h-8 border-black border-[3px] border-solid rounded-md">
+                                    
+                                </div>
+                                <div className="pr-2">
+                                    ..............................................................?
+                                </div>
+                            </div>
+                            {
+                                index !== achivments.length-1 && <hr width="84%" color="#313229" className="h-[3px] border-t-0 rounded-xl mx-6" />
+                            }
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     )
