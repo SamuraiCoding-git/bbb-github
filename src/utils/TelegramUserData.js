4f808b828cae1ffd7a547c1a9bb6ce66ef6f9ff5
K.Darlin <konstantdarl@gmail.com>
2024-06-09 14:53:58 +0300

    Fixed TelegramUserData

--- src/utils/TelegramUserData.js
+++ src/utils/TelegramUserData.js
@@ -1,1 +1,3 @@
-export const { first_name, last_name, username, id } = window.Telegram ?? window.Telegram.WebApp.initDataUnsafe.user;
+export const { first_name, last_name, username, id } = window.Telegram.WebApp.initDataUnsafe.user
+                                                        ? window.Telegram.WebApp.initDataUnsafe.user 
+                                                        : {first_name: undefined, last_name: undefined, username: undefined, id: undefined};
